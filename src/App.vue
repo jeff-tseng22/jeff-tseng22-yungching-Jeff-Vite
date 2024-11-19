9de17c24e8edbf0860bb04b1bf331a1cd2f1e35a
Jeff Tseng <jeff_tseng@aol.com>
2024-11-19 23:29:07 +0800

    add page and intall scss

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Welcome!</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+  };
+</script>
+
+<style scoped>
+</style>
---
+++ src/page/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Jeff yungching interview</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+  };
+</script>
+
+<style scoped>
+    .home {
+        padding: 20px;
+    }
+</style>
