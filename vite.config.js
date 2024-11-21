be1d606b40b4fadee8ebe30fd5d1d02ccfaf54d8
Jeff Tseng <jeff_tseng@aol.com>
2024-11-21 15:17:23 +0800

    env setting

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [
     vue(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': 'vue/dist/vue.esm-bundler.js',
     },
   },
   css: {
